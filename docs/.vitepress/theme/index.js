3b556b9cb096938e70f12866c0cad8f90344b46a
Nick Anthony Miras <mirasnickanthony@gmail.com>
2025-12-21 21:47:56 +0800

    fixed import error

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,8 +1,6 @@
 // .vitepress/theme/index.ts
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
-// Import the styles
-import '@nolebase/vitepress-plugin-page-properties/client/style.css'
 
 export default {
   ...DefaultTheme
